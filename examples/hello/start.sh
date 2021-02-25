b274c3cc4dbd3449b91ef8ad584b820f4c9a1488
mirahs <mirahs@163.com>
2021-02-25 16:18:35 +0800

    示例启动参数修改

--- examples/hello/start.sh
+++ examples/hello/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export PATH=/d/apps/erl9.2/bin:${PATH}
 
-werl -pa ebin deps/*/ebin -boot start_sasl +P 1024000 -smp enable -s hello &
+werl -pa ebin deps/*/ebin -boot start_sasl +P 1024000 -s hello &
