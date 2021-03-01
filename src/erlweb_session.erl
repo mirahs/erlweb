87661dca6cc330a7ab8ecb8e60f73cbeb3825950
mirahs <mirahs@163.com>
2021-03-01 15:43:09 +0800

    架修改(session正常

--- src/erlweb_session.erl
+++ src/erlweb_session.erl
@@ -19,8 +19,8 @@
 
 -define(dict_session_id,        dict_session_id).
 -define(dict_session_keys,		dict_session_keys).
--define(cookie_session_id,		<<"cookie_session_id">>).
--define(cookie_session_id_atom,	cookie_session_id).
+-define(SESSION_COOKIE,			<<"session_cookie">>).
+-define(SESSION_COOKIE_ATOM,	session_cookie).
 
 
 execute(Req, Env = #{handler_opts := #{session_apps := SessionApps}}) ->
@@ -28,32 +28,33 @@ execute(Req, Env = #{handler_opts := #{session_apps := SessionApps}}) ->
     AppB    = ?IF(AppBTmp =:= undefined, <<"index">>, AppBTmp),
     case lists:member(AppB, SessionApps) of
         true ->
+            ?INFO("on request"),
             erlang:erase(?dict_session_id),
-            case cowboy_req:match_cookies([{?cookie_session_id_atom, [], <<>>}], Req) of
+            case cowboy_req:match_cookies([{?SESSION_COOKIE_ATOM, [], <<>>}], Req) of
                 #{session_cookie := SessionId} when SessionId =/= <<"">> ->
                     SessionData	= erlweb_session_srv:session_get(SessionId),
-                    %?INFO("SessionId:~p", [SessionId]),
-                    %?INFO("SessionData:~p", [SessionData]),
+                    ?INFO("SessionId:~p", [SessionId]),
+                    ?INFO("SessionData:~p", [SessionData]),
                     [erlang:put(Key, Value) || {Key, Value} <- SessionData],
                     erlang:put(?dict_session_id, SessionId),
                     erlang:put(?dict_session_keys, [Key || {Key, _Data} <- SessionData]),
                     {ok, Req, Env};
                 _ ->
                     SessionId = session_id(Req),
-                    %?INFO("SessionId:~p", [SessionId]),
+                    ?INFO("SessionId:~p", [SessionId]),
                     erlang:put(?dict_session_id, SessionId),
                     erlang:put(?dict_session_keys, []),
-                    Req2 = cowboy_req:set_resp_cookie(?cookie_session_id, SessionId, Req, #{path => <<"/">>}),
+                    Req2 = cowboy_req:set_resp_cookie(?SESSION_COOKIE, SessionId, Req, #{path => <<"/">>}),
                     {ok, Req2, Env}
             end;
         false -> {ok, Req, Env}
     end.
 
 %% 返回前调用
-on_response(Req) ->
+on_response(Req) ->?INFO("on response"),
     case erlang:get(?dict_session_id) of
-        undefined -> skip;
-        SessionId -> session_set(SessionId)
+        undefined -> ?INFO("on response skip"),skip;
+        SessionId ->?INFO("on response SessionId:~p", [SessionId]), session_set(SessionId)
     end,
     Req.
 
@@ -91,7 +92,7 @@ destory(Req) ->
     SessionId = session_id(Req, false),
     del(),
     erlweb_session_srv:session_destory(SessionId),
-    cowboy_req:set_resp_cookie(?cookie_session_id, <<"">>, Req, #{path => <<"/">>}).
+    cowboy_req:set_resp_cookie(?SESSION_COOKIE, <<"">>, Req, #{path => <<"/">>}).
 
 
 
@@ -99,14 +100,14 @@ session_id(Req) ->
     session_id(Req, true).
 
 session_id(Req, true) ->
-    case cowboy_req:match_cookies([{?cookie_session_id_atom, [], <<>>}], Req) of
+    case cowboy_req:match_cookies([{?SESSION_COOKIE_ATOM, [], <<>>}], Req) of
         #{session_cookie:=SessionId} when SessionId =/= <<"">> ->
             ?TOB(SessionId);
         _ ->
             ?TOB(erlweb_session_srv:session_new())
     end;
 session_id(Req, false) ->
-    case cowboy_req:match_cookies([{?cookie_session_id_atom, [], <<>>}], Req) of
+    case cowboy_req:match_cookies([{?SESSION_COOKIE_ATOM, [], <<>>}], Req) of
         #{session_cookie:=SessionId} when SessionId =/= <<"">> ->
             ?TOB(SessionId);
         _ ->
@@ -116,5 +117,5 @@ session_id(Req, false) ->
 
 session_set(SessionId) ->
     SessionKeys	= erlang:get(?dict_session_keys),
-    SessionData	= [{Key, erlang:get(Key)} || Key <- SessionKeys],%?INFO("SessionData:~p", [SessionData]),
+    SessionData	= [{Key, erlang:get(Key)} || Key <- SessionKeys],?INFO("SessionData:~p", [SessionData]),
     erlweb_session_srv:session_set(SessionId, SessionData).
